d93a02864235c4adcc6220bada6e86ff9ee35068
Tempeus <nekugm@gmail.com>
2019-11-16 16:30:51 -0500

    CSV writing

--- venv/Scripts/Vision_AI.py
+++ venv/Scripts/Vision_AI.py
@@ -1,5 +1,6 @@
 import io
 import os
+import csv
 from google.cloud import vision
 from google.cloud.vision import types
 
@@ -9,6 +10,7 @@ client = vision.ImageAnnotatorClient()
 #must replace this with a getter for a picture
 picture_name = 'test.jpg'
 photos_directory = 'Photos'
+
 #gets the path to the targeted picture (must input the name of the picture)
 file_name = os.path.join(os.path.dirname(__file__), '..', photos_directory, picture_name)
 
@@ -22,7 +24,19 @@ image = types.Image(content=content)
 #getting the response
 objects = client.object_localization(image=image).localized_object_annotations
 
-#getting the labels
-print('Number of objects found: {}'.format(len(objects)))
-for object_ in objects:
-    print('\n{} (confidence: {})'.format(object_.name, object_.score))
+#getting the information and writting them into a csv file
+with open("output.csv", "w", newline='') as csvFile:
+    writer = csv.writer(csvFile)            #initializing the writer object
+    num_object = len(objects)
+    writer.writerow([num_object])
+    print('Number of objects found: {}'.format(num_object))
+
+    for object_ in objects:
+        name_object = object_.name
+        score_object = object_.score
+        csv_data = [name_object, score_object]
+
+        writer.writerow(csv_data)
+        print('{} (confidence: {})'.format(object_.name, object_.score))
+
+csvFile.close()
